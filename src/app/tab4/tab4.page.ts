314f60e7a2b140df89c8d77d453806c4564cf25c
KevColomaDev <kev.enrri@hotmail.com>
2024-10-28 10:20:31 -0500

    fix: saveFile

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
-
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
@@ -12,21 +11,32 @@ export class Tab4Page {
   constructor() {}
 
   // Function to save the text to a file
-  saveText() {
+  async saveText() {
     if (this.texto.trim().length === 0) {
       return;
     }
-    // Save the text to the file
-    Filesystem.writeFile({
-      path: 'Prueba.txt',
-      data: this.texto,
-      directory: Directory.Documents,
-      encoding: Encoding.UTF8
-    }).then(() => {
-      alert('Texto guardado');
+
+    try {
+      // Crear una carpeta llamada 'miCarpeta'
+      await Filesystem.mkdir({
+        path: 'miCarpeta',
+        directory: Directory.Documents,
+        recursive: true // Permite crear carpetas anidadas
+      });
+
+      // Guardar el texto en el archivo dentro de la carpeta
+      await Filesystem.writeFile({
+        path: 'miCarpeta/prueba.txt', // Ruta del archivo dentro de la carpeta
+        data: this.texto,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8
+      });
+
+      alert('Texto guardado en miCarpeta/prueba.txt');
       this.texto = '';
-    }).catch(() => {
-      alert('Error al guardar el texto');
-    });
+    } catch (err) {
+      console.log(err);
+      alert('Error al guardar el texto: ' + err);
+    }
   }
 }
