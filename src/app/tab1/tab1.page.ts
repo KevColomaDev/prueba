fd550fe2b83c8e0a86fadb125dcabc81b789df7a
KevColomaDev <kev.enrri@hotmail.com>
2024-10-27 23:28:46 -0500

    feat: more components tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { ItemReorderEventDetail, OverlayEventDetail } from '@ionic/core';
 import { addIcons } from 'ionicons';
 import { close, closeCircle, pin} from 'ionicons/icons';
 
@@ -8,6 +10,10 @@ import { close, closeCircle, pin} from 'ionicons/icons';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonModal) modal?: IonModal;
+
+  message?: string;
+  name?: string;
 
   constructor() {
     addIcons({
@@ -20,4 +26,39 @@ export class Tab1Page {
     window.open(url, '_blank');
   }
 
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    // Aquí puedes manejar cualquier lógica que necesites al cerrar el modal
+    console.log('Modal will dismiss', event);
+  }
+
+  // Método para cancelar y cerrar el modal
+  cancel() {
+    if (this.modal) {
+      this.modal.dismiss(null, 'cancel');
+    }
+  }
+
+  // Método para confirmar y manejar la entrada del usuario
+  confirm() {
+    if (this.modal) {
+      this.message = `Hola, ${this.name}!, que te trae por aqui?`;
+      this.modal.dismiss(this.name, 'confirm');
+    }
+  }
+  handleRefresh(event: { target: { complete: () => void; }; }) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
+  }
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
 }
